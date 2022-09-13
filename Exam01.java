6407026e46fa416dd4abd9e25aff292e91487fd9
mingi0416 <70953813+mingi0416@users.noreply.github.com>
2022-09-13 17:26:56 +0900

    2022-09-13 Java 수업 후 문제풀이 내용

---
+++ Exam01.java
@@ -0,0 +1,122 @@
+package com.human.ex;
+
+public class Exam01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		
+		System.out.print("1");
+		System.out.print("2");
+		System.out.print("3");
+		
+		System.out.print("hello");
+		System.out.println("World");
+		
+		System.out.print("12");
+		System.out.println("34");
+		System.out.println("56\n");
+
+		
+		System.out.println("hello");
+		System.out.println("world\n");
+		
+		System.out.print("  ");
+		System.out.print("1");
+		System.out.println();
+		System.out.print(" ");
+		System.out.print("1");
+		System.out.print("2");
+		System.out.println();
+		System.out.print("1");
+		System.out.print("2");
+		System.out.print("3");
+		System.out.println("\n");
+		
+		System.out.print("****");
+		System.out.println();
+		System.out.print("****");
+		System.out.println();
+		System.out.print("****");
+		System.out.println();
+		System.out.print("****");
+		System.out.println("\n");
+		
+		System.out.print("*");
+		System.out.print("   ");
+		System.out.println("");
+		System.out.print("**");
+		System.out.print("  ");
+		System.out.println("");
+		System.out.print("***");
+		System.out.print(" ");
+		System.out.println();
+		System.out.print("****");
+		System.out.println("\n");
+		
+		System.out.print("   ");
+		System.out.print("*");
+		System.out.println("");
+		System.out.print("  ");
+		System.out.print("**");
+		System.out.println("");
+		System.out.print(" ");
+		System.out.print("***");
+		System.out.println();
+		System.out.print("****");
+		System.out.println("\n");
+		
+		System.out.print("  ");
+		System.out.print("*");
+		System.out.println();
+		System.out.print(" ");
+		System.out.print("***");
+		System.out.println();
+		System.out.print("*****");
+		System.out.println();
+		System.out.print(" ");
+		System.out.print("***");
+		System.out.println();
+		System.out.print("  ");
+		System.out.print("*");
+		System.out.println("\n");
+		
+		System.out.println("'안녕자바'");
+		System.out.println("\n");
+		
+		System.out.println("-------------------------");
+		System.out.println("ㅣ교육센터                ㅣ");
+		System.out.println("ㅣ학생ㅡ김민기             ㅣ");
+		System.out.println("ㅣmingi0416@gmail.com   ㅣ");
+		System.out.println("ㅣ010-1234-5678         ㅣ");
+		System.out.println("-------------------------");
+		System.out.println("\n");
+		
+		
+		System.out.println("*****************");
+		System.out.println("*     성적표      *");
+		System.out.println("*****************");
+		System.out.println("* 국어     50    *");
+		System.out.println("*****************");
+		System.out.println("* 영어     50    *");
+		System.out.println("*****************");
+		System.out.println("* 수학     50    *");
+		System.out.println("*****************");
+		System.out.println("* 컴퓨터 학과 홍길동*");
+		System.out.println("*****************");
+		System.out.println("\n");
+		
+		System.out.println("일\t월\t화\t수\t목\t금\t토");
+		System.out.println("1\t2\t3\t4\t5\t6\t7");
+		System.out.println("8\t9\t10\t11\t12\t13\t14");
+		System.out.println("15\t16\t17\t18\t19\t20\t21");
+		System.out.println("22\t23\t24\t25\t26\t27\t28");
+		System.out.println("29\t30\t31");
+		System.out.println("\n");
+
+		
+	}
+
+}
